e77e0e970ea6df7fdefe1b58015d352a5d820bce
yongsik <53222549+horayong@users.noreply.github.com>
2025-06-13 01:17:37 +0900

    로비 스크립트 구현중

--- Assets/Scripts/Lobby/Lobby.cs
+++ Assets/Scripts/Lobby/Lobby.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Lobby : MonoBehaviour
+public class Lobby : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("---UI InputFields---")]
+    public TMP_InputField createRoom;
+    public TMP_InputField joinRoom;
+    private void Awake()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    public override void OnJoinedLobby()
+    {
+
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        int roomName = Random.Range(0, 10000);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.CreateRoom(roomName.ToString(), roomOptions, TypedLobby.Default, null);
+    }
+    public void OnClickCreateButton()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        //PhotonNetwork.CreateRoom(createRoom.text, roomOptions, typedLobby.Default, null);
     }
 }
